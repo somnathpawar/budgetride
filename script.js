36f92448dcb4a19e4c7c3146bddad354bb182a30
Somnath Lalaso Pawar <somnath.pawar@tech9.com>
2018-09-18 09:30:10 +0530

    code indent

--- script.js
+++ script.js
@@ -228,41 +228,42 @@ $(function() {
        });
   });
 
-//Sort table on header click in ascending and descending order.
+  //Sort table on header click in ascending and descending order.
   function sortTable(f,n){
-	var rows = $('.data_table tbody tr:gt(0)').get();
+    var rows = $('.data_table tbody tr:gt(0)').get();
 
-	rows.sort(function(a, b) {
+    rows.sort(function(a, b) {
 
-		var A = getVal(a);
-		var B = getVal(b);
+      var A = getVal(a);
+      var B = getVal(b);
 
-		if(A < B) {
-			return -1*f;
-		}
-		if(A > B) {
-			return 1*f;
-		}
-		return 0;
-	});
+      if(A < B) {
+        return -1*f;
+      }
+      if(A > B) {
+        return 1*f;
+      }
+      return 0;
+    });
 
-	function getVal(elm){
-		var v = $(elm).children('td').eq(n).text().toUpperCase();
-		if($.isNumeric(v)){
-			v = parseInt(v,10);
-		}
-		return v;
-	}
+    function getVal(elm){
+      var v = $(elm).children('td').eq(n).text().toUpperCase();
+      if($.isNumeric(v)){
+        v = parseInt(v,10);
+      }
+      return v;
+    }
 
-	$.each(rows, function(index, row) {
-		$('.data_table').children('tbody').append(row);
-	});
-}
-var f_sl = 1;
-var f_nm = 1;
-$("th#sorted_field").click(function(){
-    f_sl *= -1;
-    var n = $(this).prevAll().length;
-    sortTable(f_sl,n);
-});
+    $.each(rows, function(index, row) {
+      $('.data_table').children('tbody').append(row);
+    });
+  }
+
+  var f_sl = 1;
+  var f_nm = 1;
+  $("th#sorted_field").click(function(){
+      f_sl *= -1;
+      var n = $(this).prevAll().length;
+      sortTable(f_sl,n);
+  });
 });
